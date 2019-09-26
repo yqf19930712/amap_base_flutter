0286d6c86655246119e4f7935df7826419aafbb7
Yang Qinfeng <389549807@qq.com>
2019-09-26 15:08:38 +0800

    fix compile problem

--- map/android/src/main/kotlin/me/yohom/amapbasemap/map/MapHandlers.kt
+++ map/android/src/main/kotlin/me/yohom/amapbasemap/map/MapHandlers.kt
@@ -240,7 +240,7 @@ object OpenOfflineManager : MapMethodHandler {
         return this
     }
 
-    override fun onMethodCall(p0: MethodCall?, p1: MethodChannel.Result?) {
+    override fun onMethodCall(p0: MethodCall, p1: MethodChannel.Result) {
         registrar.activity().startActivity(
                 Intent(AMapBaseMapPlugin.registrar.activity(),
                         OfflineMapActivity::class.java)
